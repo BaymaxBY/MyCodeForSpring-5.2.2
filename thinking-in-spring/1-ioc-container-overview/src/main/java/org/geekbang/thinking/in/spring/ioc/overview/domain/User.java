5d225a962ff7e32f50aad5ee85fa438da0b1062d
BaymaxBY <17621133652@163.com>
2021-01-10 23:52:56 +0800

    1-ioc-container-overview，2-dependency-lookup

--- thinking-in-spring/1-ioc-container-overview/src/main/java/org/geekbang/thinking/in/spring/ioc/overview/domain/User.java
+++ thinking-in-spring/1-ioc-container-overview/src/main/java/org/geekbang/thinking/in/spring/ioc/overview/domain/User.java
@@ -32,13 +32,13 @@ import java.util.Properties;
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
  * @since
  */
-public class User implements BeanNameAware {
+public class User /*implements BeanNameAware*/ {
 
     private Long id;
 
     private String name;
 
-    private City city;
+    /*private City city;
 
     private City[] workCities;
 
@@ -52,10 +52,10 @@ public class User implements BeanNameAware {
 
     private String contextAsText;
 
-    /**
+    *//**
      * 当前 Bean 的名称
-     */
-    private transient String beanName;
+     *//*
+    private transient String beanName;*/
 
     public Long getId() {
         return id;
@@ -73,97 +73,105 @@ public class User implements BeanNameAware {
         this.name = name;
     }
 
-    public City getCity() {
-        return city;
-    }
-
-    public void setCity(City city) {
-        this.city = city;
-    }
-
-    public Resource getConfigFileLocation() {
-        return configFileLocation;
-    }
-
-    public void setConfigFileLocation(Resource configFileLocation) {
-        this.configFileLocation = configFileLocation;
-    }
-
-    public City[] getWorkCities() {
-        return workCities;
-    }
-
-    public void setWorkCities(City[] workCities) {
-        this.workCities = workCities;
-    }
-
-    public List<City> getLifeCities() {
-        return lifeCities;
-    }
-
-    public void setLifeCities(List<City> lifeCities) {
-        this.lifeCities = lifeCities;
-    }
-
-    public Company getCompany() {
-        return company;
-    }
-
-    public void setCompany(Company company) {
-        this.company = company;
-    }
-
-    public static User createUser() {
-        User user = new User();
-        user.setId(1L);
-        user.setName("小马哥");
-        return user;
-    }
-
-    @PostConstruct
-    public void init() {
-        System.out.println("User Bean [" + beanName + "] 初始化...");
-    }
-
-    @PreDestroy
-    public void destroy() {
-        System.out.println("User Bean [" + beanName + "] 销毁中...");
-    }
-
-    @Override
-    public void setBeanName(String name) {
-        this.beanName = name;
-    }
-
-    public Properties getContext() {
-        return context;
-    }
-
-    public void setContext(Properties context) {
-        this.context = context;
-    }
-
-    public String getContextAsText() {
-        return contextAsText;
-    }
-
-    public void setContextAsText(String contextAsText) {
-        this.contextAsText = contextAsText;
-    }
+//    public City getCity() {
+//        return city;
+//    }
+//
+//    public void setCity(City city) {
+//        this.city = city;
+//    }
+//
+//    public Resource getConfigFileLocation() {
+//        return configFileLocation;
+//    }
+//
+//    public void setConfigFileLocation(Resource configFileLocation) {
+//        this.configFileLocation = configFileLocation;
+//    }
+//
+//    public City[] getWorkCities() {
+//        return workCities;
+//    }
+//
+//    public void setWorkCities(City[] workCities) {
+//        this.workCities = workCities;
+//    }
+//
+//    public List<City> getLifeCities() {
+//        return lifeCities;
+//    }
+//
+//    public void setLifeCities(List<City> lifeCities) {
+//        this.lifeCities = lifeCities;
+//    }
+//
+//    public Company getCompany() {
+//        return company;
+//    }
+//
+//    public void setCompany(Company company) {
+//        this.company = company;
+//    }
+//
+//    public static User createUser() {
+//        User user = new User();
+//        user.setId(1L);
+//        user.setName("小马哥");
+//        return user;
+//    }
+//
+//    @PostConstruct
+//    public void init() {
+//        System.out.println("User Bean [" + beanName + "] 初始化...");
+//    }
+//
+//    @PreDestroy
+//    public void destroy() {
+//        System.out.println("User Bean [" + beanName + "] 销毁中...");
+//    }
+//
+//    @Override
+//    public void setBeanName(String name) {
+//        this.beanName = name;
+//    }
+//
+//    public Properties getContext() {
+//        return context;
+//    }
+//
+//    public void setContext(Properties context) {
+//        this.context = context;
+//    }
+//
+//    public String getContextAsText() {
+//        return contextAsText;
+//    }
+//
+//    public void setContextAsText(String contextAsText) {
+//        this.contextAsText = contextAsText;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "User{" +
+//                "id=" + id +
+//                ", name='" + name + '\'' +
+//                ", city=" + city +
+//                ", workCities=" + Arrays.toString(workCities) +
+//                ", lifeCities=" + lifeCities +
+//                ", configFileLocation=" + configFileLocation +
+//                ", company=" + company +
+//                ", context=" + context +
+//                ", contextAsText='" + contextAsText + '\'' +
+//                ", beanName='" + beanName + '\'' +
+//                '}';
+//    }
 
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
-                ", city=" + city +
-                ", workCities=" + Arrays.toString(workCities) +
-                ", lifeCities=" + lifeCities +
-                ", configFileLocation=" + configFileLocation +
-                ", company=" + company +
-                ", context=" + context +
-                ", contextAsText='" + contextAsText + '\'' +
-                ", beanName='" + beanName + '\'' +
                 '}';
     }
 }
